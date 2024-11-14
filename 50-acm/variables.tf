213c756b3229e9b97ea9b31403f109dd614ad0b5
lakshmimungara <lakshmimungara2997@gmail.com>
2024-11-15 05:00:06 +0530

    k8-terraform

---
+++ 50-acm/variables.tf
@@ -0,0 +1,25 @@
+
+variable "project_name" {
+    default = "expense"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "common_tags" {
+    default = {
+        Project = "expense"
+        Terraform = "true"
+        Environment = "dev"
+    }
+}
+
+
+variable "zone_name" {
+    default = "daws81s.fun"
+}
+
+variable "zone_id" {
+    default = "Z09912121MS725XSKH1TG"
+}
